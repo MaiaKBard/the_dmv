a712f9963a4aa61a3f56c28c4029f9eea1a7e3f0
Maia Bard <Maiakbard@gmail.com>
2025-02-12 19:48:29 -0700

    Add method permit? and earn_permit to code and test files

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -9,7 +9,28 @@ RSpec.describe Registrant do
     end
 
     it 'exists' do
+        expect(@registrant).to be_a(Registrant)
+    end
 
+    it 'has a name' do
+        expect(@registrant.name).to eq("Bruce")
+    end
 
+    it 'has an age' do
+        expect(@registrant.age).to eq(18)
+    end
+
+    it 'has a permit' do
+        expect(@registrant.permit?).to eq(true)
+    end
+
+    it 'contains data on license' do
+        expect(@registrant.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    end
+
+    it 'can earn a permit' do
+        expect(@registrant_2.permit?).to eq(false)
+        @registrant_2.earn_permit
+        expect(@registrant_2.permit).to eq(true)
     end
 end
