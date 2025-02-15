b7dbb1f8f318b626190f8c4ad9c51b708c998920
Maia Bard <Maiakbard@gmail.com>
2025-02-15 09:35:22 -0700

    Add 'can add servies per facility' and 'has each facility hold their registered vehicles'

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,12 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(details)
     @name = details[:name]
     @address = details[:address]
     @phone = details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
